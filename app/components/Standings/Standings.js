7f58bcd4f04b116e91ac7b120f9bd2a0782bafd7
Zac Arellano <zaccc@email.arizona.edu>
2016-07-29 15:24:43 -0700

    moved all team color data to teams.css file

--- app/components/Standings/Standings.js
+++ app/components/Standings/Standings.js
@@ -1,46 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import { Loading } from 'components'
-import { header, standingsContainer, row, teamLogo, filterRow, nyy,
-  xLarge, large, medium, xxLarge, filterList, filterItem, loadingContainer } from './styles.css'
+import { header, standingsContainer, row, teamLogo, filterRow, xLarge, large, medium, xxLarge, filterList, filterItem, loadingContainer } from './styles.css'
+import * as teamColors from './teams.css'
 
 class TeamRow extends React.Component {
   render() {
     let teamData = this.props.team
-    var teamColors = {
-      ana: [{ backgroundImage: '-webkit-linear-gradient(0deg, #ba0021 20%, transparent 0%)' }],
-      ari: [{ backgroundImage: '-webkit-linear-gradient(0deg, #000000 20%, transparent 0%)' }],
-      atl: [{ backgroundImage: '-webkit-linear-gradient(0deg, #13274f 20%, transparent 0%)' }],
-      bal: [{ backgroundImage: '-webkit-linear-gradient(0deg, #000000 20%, transparent 0%)' }],
-      bos: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0d2b56 20%, transparent 0%)' }],
-      chc: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0e3386 20%, transparent 0%)' }],
-      cin: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c6011f 20%, transparent 0%)' }],
-      cle: [{ backgroundImage: '-webkit-linear-gradient(0deg, #002b5c 20%, transparent 0%)' }],
-      col: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c4ced4 20%, transparent 0%)' }],
-      cws: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c4ced4 20%, transparent 0%)' }],
-      det: [{ backgroundImage: '-webkit-linear-gradient(0deg, #ffffff 20%, transparent 0%)' }],
-      hou: [{ backgroundImage: '-webkit-linear-gradient(0deg, #002d62 20%, transparent 0%)' }],
-      kc:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #ffffff 20%, transparent 0%)' }],
-      la:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #ffffff 20%, transparent 0%)' }],
-      mia: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0077c8 20%, transparent 0%)' }],
-      mil: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0a2351 20%, transparent 0%)' }],
-      min: [{ backgroundImage: '-webkit-linear-gradient(0deg, #d31145 20%, transparent 0%)' }],
-      nym: [{ backgroundImage: '-webkit-linear-gradient(0deg, #002d72 20%, transparent 0%)' }],
-      nyy: [{ null }],
-      oak: [{ backgroundImage: '-webkit-linear-gradient(0deg, #003831 20%, transparent 0%)' }],
-      phi: [{ backgroundImage: '-webkit-linear-gradient(0deg, #e81828 20%, transparent 0%)' }],
-      pit: [{ backgroundImage: '-webkit-linear-gradient(0deg, #fdb827 20%, transparent 0%)' }],
-      sd:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #7f411c 20%, transparent 0%)' }],
-      sea: [{ backgroundImage: '-webkit-linear-gradient(0deg, #005c5c 20%, transparent 0%)' }],
-      sf:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #000000 20%, transparent 0%)' }],
-      stl: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c41e3a 20%, transparent 0%)' }],
-      tb:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #8fbce6 20%, transparent 0%)' }],
-      tex: [{ backgroundImage: '-webkit-linear-gradient(0deg, #003278 20%, transparent 0%)' }],
-      tor: [{ backgroundImage: '-webkit-linear-gradient(0deg, #134a8e 20%, transparent 0%)' }],
-      was: [{ backgroundImage: '-webkit-linear-gradient(0deg, #ab0003 20%, transparent 0%)' }]
-    }
     return (
-      <div className={teamData.file_code === 'nyy' ? nyy : row} style={teamColors[`${teamData.file_code}`][0]}>
+      <div className={teamColors[teamData.file_code]}>
         <img className={teamLogo} src={`assets/img/mlb/teams/${teamData.file_code}.svg`} alt={teamData.team_full} />
         <span className={xLarge}>{teamData.team_short}</span>
         <span className={medium}>{teamData.w}</span>
--- app/components/Team/Team.js
+++ app/components/Team/Team.js
@@ -1,42 +1,10 @@
 import React from 'react'
-import { teamContainer, teamLogo, teamInfo, teamName,
-  teamRecord, teamScore, nyy } from './styles.css'
+import { container, teamLogo, teamInfo, teamName, teamRecord, teamScore } from './styles.css'
+import * as teamColors from './colors.css'
 
 export default function Team({name, code, ls, ws, runs}) {
-  var teamColors = {
-    ana: [{ backgroundImage: '-webkit-linear-gradient(0deg, #ba0021 40%, transparent 0%)' }],
-    ari: [{ backgroundImage: '-webkit-linear-gradient(0deg, #000000 40%, transparent 0%)' }],
-    atl: [{ backgroundImage: '-webkit-linear-gradient(0deg, #13274f 40%, transparent 0%)' }],
-    bal: [{ backgroundImage: '-webkit-linear-gradient(0deg, #000000 40%, transparent 0%)' }],
-    bos: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0d2b56 40%, transparent 0%)' }],
-    chc: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0e3386 40%, transparent 0%)' }],
-    cin: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c6011f 40%, transparent 0%)' }],
-    cle: [{ backgroundImage: '-webkit-linear-gradient(0deg, #002b5c 40%, transparent 0%)' }],
-    col: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c4ced4 40%, transparent 0%)' }],
-    cws: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c4ced4 40%, transparent 0%)' }],
-    det: [{ backgroundImage: '-webkit-linear-gradient(0deg, #ffffff 40%, transparent 0%)' }],
-    hou: [{ backgroundImage: '-webkit-linear-gradient(0deg, #002d62 40%, transparent 0%)' }],
-    kc:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #ffffff 40%, transparent 0%)' }],
-    la:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #ffffff 40%, transparent 0%)' }],
-    mia: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0077c8 40%, transparent 0%)' }],
-    mil: [{ backgroundImage: '-webkit-linear-gradient(0deg, #0a2351 40%, transparent 0%)' }],
-    min: [{ backgroundImage: '-webkit-linear-gradient(0deg, #d31145 40%, transparent 0%)' }],
-    nym: [{ backgroundImage: '-webkit-linear-gradient(0deg, #002d72 40%, transparent 0%)' }],
-    nyy: [{ null }],
-    oak: [{ backgroundImage: '-webkit-linear-gradient(0deg, #003831 40%, transparent 0%)' }],
-    phi: [{ backgroundImage: '-webkit-linear-gradient(0deg, #e81828 40%, transparent 0%)' }],
-    pit: [{ backgroundImage: '-webkit-linear-gradient(0deg, #fdb827 40%, transparent 0%)' }],
-    sd:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #7f411c 40%, transparent 0%)' }],
-    sea: [{ backgroundImage: '-webkit-linear-gradient(0deg, #005c5c 40%, transparent 0%)' }],
-    sf:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #000000 40%, transparent 0%)' }],
-    stl: [{ backgroundImage: '-webkit-linear-gradient(0deg, #c41e3a 40%, transparent 0%)' }],
-    tb:  [{ backgroundImage: '-webkit-linear-gradient(0deg, #8fbce6 40%, transparent 0%)' }],
-    tex: [{ backgroundImage: '-webkit-linear-gradient(0deg, #003278 40%, transparent 0%)' }],
-    tor: [{ backgroundImage: '-webkit-linear-gradient(0deg, #134a8e 40%, transparent 0%)' }],
-    was: [{ backgroundImage: '-webkit-linear-gradient(0deg, #ab0003 40%, transparent 0%)' }]
-  }
     return (
-    <div className={code === 'nyy' ? nyy : teamContainer} style={teamColors[`${code}`][0]}>
+    <div className={teamColors[code]}>
       <img className={teamLogo} src={`assets/img/mlb/teams/${code}.svg`} alt={name} />
       <div className={teamInfo}>
         <span className={teamName}>{name.length > 7 ? <small>{name}</small> : name}</span>

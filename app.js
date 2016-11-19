0d47fa740042f4da72bbb4517fc484ad05af6900
Zac Arellano <zaccc@email.arizona.edu>
2016-11-18 21:07:18 -0700

    fix app.js + add dist files to test on express

---
+++ app.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const express = require('express')
+const compression = require('compression')
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(compression())
+app.use(express.static('dist'))
+app.listen(port, function() {
+  console.log(`server listen on port ${port}`)
+})
+
+module.exports = app

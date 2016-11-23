67087ed432f6000102012d4cb3df98d08296b2e1
Zac Arellano <zaccc@email.arizona.edu>
2016-11-22 17:00:50 -0700

    add mr_robot 404//NotFound image

--- app/components/NotFound/NotFound.js
+++ app/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import { container, header } from './styles.css'
 export default function NotFound() {
   return (
     <div className={container}>
-      <h1 className={header}>{'404'}</h1>
+      <img src='/assets/img/mr_robot.png' alt='' />
     </div>
   )
 }

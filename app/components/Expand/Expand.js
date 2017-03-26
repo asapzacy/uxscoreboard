0c7fed48d2356aa8a38db29be317e8483b5d1279
Zac Arellano <zaccc@email.arizona.edu>
2017-03-25 21:27:09 -0700

    fix <Expand /> icon from moving around

--- app/components/Expand/Expand.js
+++ app/components/Expand/Expand.js
@@ -2,10 +2,10 @@ import React from 'react'
 import AngleDown from 'react-icons/lib/fa/angle-down'
 import { iconContainer, expandedIcon, icon } from './styles.css'
 
-export default function Expand({ expanded, showDetails }) {
-  return (
-    <span className={expanded ? expandedIcon : iconContainer} onClick={showDetails}>
-      <span className={icon}><AngleDown /></span>
-    </span>
-  )
-}
+const Expand = ({ expanded, showDetails }) => (
+  <span className={expanded ? expandedIcon : iconContainer} onClick={showDetails}>
+    <span className={icon}><AngleDown /></span>
+  </span>
+)
+
+export default Expand

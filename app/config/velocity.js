2df6025ecac9a9d54e2cd7c58ae028df31aec8ca
Zac Arellano <zaccc@email.arizona.edu>
2017-03-27 20:09:49 -0700

    ay css

--- app/config/velocity.js
+++ app/config/velocity.js
@@ -4,15 +4,22 @@ export const velocity = (direction) => ({
   enter: {
     animation: `transition.slide${direction.enter}${direction.enter !== 'Up' ? 'Big' : ''}In`,
     duration: direction.enter === 'Up' ? 1220 : 880,
-    delay: direction.enter === 'Up' ? 0 : 680,
+    delay: direction.enter === 'Up' ? 0 : 580,
     display: 'flex',
-    style: { position: 'relative', top: 0, left: 0, right: 0 }
+    style: {
+      position: 'relative',
+      top: 0,
+      left: 0,
+      right: 0
+    }
   },
   runOnMount: true,
   leave: {
     animation: `transition.slide${direction.leave}BigOut`,
     duration: 880,
     display: 'none',
-    style: { position: 'absolute' }
+    style: {
+      position: 'absolute'
+    }
   }
 })

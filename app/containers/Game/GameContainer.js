bc2435febc44b72e4a047ff0d1ff01a1677e40c9
Zac Arellano <zaccc@email.arizona.edu>
2017-04-01 22:42:18 -0700

    add isiOS state to <GameContainer />

--- app/containers/Game/GameContainer.js
+++ app/containers/Game/GameContainer.js
@@ -7,19 +7,19 @@ class GameContainer extends Component {
     this.state = {
       isExpanded: false,
       isHovered: false,
-      isSafari: false
+      isiOS: false
     }
     this.showDetails = this.showDetails.bind(this)
     this.scaleGame = this.scaleGame.bind(this)
   }
   componentDidMount() {
-    this.setState({ isSafari: /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor) })
+    this.setState({ isiOS: /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream })
   }
   showDetails() {
     this.setState({ isExpanded: !this.state.isExpanded })
   }
   scaleGame() {
-    this.setState({ isHovered: !this.state.isSafari && !this.state.isHovered })
+    this.setState({ isHovered: !this.state.isiOS && !this.state.isHovered })
   }
   render() {
     return <Game showDetails={this.showDetails} scaleGame={this.scaleGame} {...this.state} {...this.props} />

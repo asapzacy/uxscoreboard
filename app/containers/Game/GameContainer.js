f3b287a2957918413024241479e548b67aa6b0b5
Zac Arellano <zaccc@email.arizona.edu>
2017-04-01 21:33:24 -0700

    good old safari (mobile touch issue)

--- app/containers/Game/GameContainer.js
+++ app/containers/Game/GameContainer.js
@@ -6,16 +6,20 @@ class GameContainer extends Component {
     super()
     this.state = {
       isExpanded: false,
-      isHovered: false
+      isHovered: false,
+      isSafari: false
     }
     this.showDetails = this.showDetails.bind(this)
     this.scaleGame = this.scaleGame.bind(this)
+  }
+  componentDidMount() {
+    this.setState({ isSafari: /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor) })
   }
   showDetails() {
     this.setState({ isExpanded: !this.state.isExpanded })
   }
   scaleGame() {
-    this.setState({ isHovered: !this.state.isHovered })
+    this.setState({ isHovered: !this.state.isSafari && !this.state.isHovered })
   }
   render() {
     return <Game showDetails={this.showDetails} scaleGame={this.scaleGame} {...this.state} {...this.props} />

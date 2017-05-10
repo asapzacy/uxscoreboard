d689ee05e4e815ff9a748a1f0b9eb6f3292ae1a4
Zac Arellano <zaccc@email.arizona.edu>
2017-05-09 18:51:14 -0700

    fix San Antonion Spurs stadium name from 'ATamp;T Center' --> 'AT&T Center'

--- app/components/Matchup/props.js
+++ app/components/Matchup/props.js
@@ -1,34 +1,27 @@
 
 //  mlb matchup props --> Matchup component
-export const mlbMatchupProps = (game, date) => {
-  return {
-    awayTeam: game.teams.away.team.teamName,
-    homeTeam: game.teams.home.team.teamName,
-    location: game.teams.home.team.locationName,
-    venue: game.venue.name,
-    date
-  }
-}
+export const mlbMatchupProps = (game, date) => ({
+  awayTeam: game.teams.away.team.teamName,
+  homeTeam: game.teams.home.team.teamName,
+  location: game.teams.home.team.locationName,
+  venue: game.venue.name,
+  date
+})
 
 //  nba matchup props --> Matchup component
-export const nbaMatchupProps = (game, date) => {
-  return {
-    awayTeam: game.visitor.nickname,
-    homeTeam: game.home.nickname,
-    location: `${game.city}, ${game.state}.`,
-    venue: game.arena,
-    date
-  }
-}
+export const nbaMatchupProps = (game, date) => ({
+  awayTeam: game.visitor.nickname,
+  homeTeam: game.home.nickname,
+  location: `${game.city}, ${game.state}.`,
+  venue: game.arena === 'AT&amp;T Center' ? 'AT&T Center' : game.arean,
+  date
+})
 
 //  nhl matchup props --> Matchup component
-export const nhlMatchupProps = (game, date) => {
-  const isAllStar = game.gameType === 'A'
-  return {
-    awayTeam: game.teams.away.team.teamName,
-    homeTeam: game.teams.home.team.teamName,
-    location: isAllStar ? 'Los Angeles' : game.teams.home.team.venue.city,
-    venue: game.venue.name,
-    date
-  }
-}
+export const nhlMatchupProps = (game, date) => ({
+  awayTeam: game.teams.away.team.teamName,
+  homeTeam: game.teams.home.team.teamName,
+  location: game.gameType === 'A' ? 'Los Angeles' : game.teams.home.team.venue.city,
+  venue: game.venue.name,
+  date
+})

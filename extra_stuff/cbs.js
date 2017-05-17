a0ad78731454c282d31bd5961405b1341dae4e2a
Zac Arellano <zaccc@email.arizona.edu>
2017-05-17 16:06:28 -0700

    styling for mobile

---
+++ extra_stuff/cbs.js
@@ -0,0 +1,4 @@
+
+
+const pri = `http://sportsfly.cbsistatic.com/bundles/sportsmediacss/images/team-logos/${leagye}/${code.toUpperCase()}.svg`
+const alt = `http://sportsfly.cbsistatic.com/bundles/sportsmediacss/images/team-logos/${league}/alt/${code.toUpperCase()}.svg`

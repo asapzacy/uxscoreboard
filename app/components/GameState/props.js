57a811551b5945a2d8674533e03bed765d5b7524
Zac Arellano <zaccc@email.arizona.edu>
2017-05-26 15:27:37 -0700

    fix rare edge case coming from MLB api

--- app/components/GameState/props.js
+++ app/components/GameState/props.js
@@ -12,11 +12,11 @@ export const mlbGameStateProps = (game) => {
   const hasStarted = (inGame || isOver) && !isDelayed
   const inningState = inGame ? game.linescore.inningState : ''
   const inBetween = inningState && inningState !== 'Middle' && inningState !== 'End'
-  const time = `${formatTimezone(game.gameDate)} ET`
+  const time = isTBD ? 'TBD' : `${formatTimezone(game.gameDate)} ET`
   return {
     gameState: inGame ? 1 : isOver && !isDelayed ? 2 : 0,
     status: inGameDelay ? `${game.status.detailedState} - (${game.status.reason})` : game.status.abstractGameState,
-    time: isDelayed ? `${time} - ${game.status.detailedState} (${game.status.reason.split(' ').pop()})` : isTBD ? 'TBD' : time,
+    time: isDelayed ? `${time} - ${game.status.detailedState} ${game.status.reason !== undefined ? `(${game.status.reason.split(' ')})` : ''}` : time,
     periods: 9,
     currentTime: inBetween && `${game.linescore.outs} ${game.linescore.outs === 1 ? 'out' : 'outs'}`,
     currentPeriod: inGame && `${inningState === 'End' ? inningState.toUpperCase() : inningState} ${game.linescore.currentInning}`,

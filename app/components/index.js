26faa212ad6d09b2dded374605ef559d1af1d085
Zac Arellano <zacdarellano@gmail.com>
2017-08-19 19:40:18 -0700

    Changed the import/export syntax of all components + containers.

--- app/components/index.js
+++ app/components/index.js
@@ -1,36 +1,63 @@
+
 /* -------  base components  ------- */
-export Header from './Header/Header'
-export Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Social from './Social/Social'
 
 /* -------  page components  ------- */
-export Home from './Home/Home'
-export About from './About/About'
-export NotFound from './NotFound/NotFound'
-export Test from './Test/Test'
-
-/* -------  random components  ------- */
-export Social from './Social/Social'
+import Home from './Home/Home'
+import About from './About/About'
+import NotFound from './NotFound/NotFound'
+import Test from './Test/Test'
 
 /* -------  league components  ------- */
-export League from './League/League'
-export Loading from './Loading/Loading'
-export NoGames from './NoGames/NoGames'
+import League from './League/League'
+import Loading from './Loading/Loading'
+import NoGames from './NoGames/NoGames'
 
 /* -------  scoreboard components  ------- */
-export Scoreboard from './Scoreboard/Scoreboard'
-export Date from './Date/Date'
-export Event from './Event/Event'
+import Scoreboard from './Scoreboard/Scoreboard'
+import Date from './Date/Date'
+import Event from './Event/Event'
 
 /* -------  game components  ------- */
-export Game from './Game/Game'
-export GameState from './GameState/GameState'
-export Team from './Team/Team'
-export Logo from './Logo/Logo'
-export Expand from './Expand/Expand'
-export Details from './Details/Details'
-export Matchup from './Matchup/Matchup'
-export PanelMenu from './PanelMenu/PanelMenu'
-export BoxScore from './BoxScore/BoxScore'
-export Stats from './Stats/Stats'
-export Diamond from './Diamond/Diamond'
-export UpdateTime from './UpdateTime/UpdateTime'
+import Game from './Game/Game'
+import GameState from './GameState/GameState'
+import Team from './Team/Team'
+import Logo from './Logo/Logo'
+import Expand from './Expand/Expand'
+import Details from './Details/Details'
+import Matchup from './Matchup/Matchup'
+import PanelMenu from './PanelMenu/PanelMenu'
+import BoxScore from './BoxScore/BoxScore'
+import Stats from './Stats/Stats'
+import Diamond from './Diamond/Diamond'
+import UpdateTime from './UpdateTime/UpdateTime'
+
+export {
+  Header,
+  Footer,
+  Social,
+  Home,
+  About,
+  NotFound,
+  Test,
+  League,
+  Loading,
+  NoGames,
+  Scoreboard,
+  Date,
+  Event,
+  Game,
+  GameState,
+  Team,
+  Logo,
+  Expand,
+  Details,
+  Matchup,
+  PanelMenu,
+  BoxScore,
+  Stats,
+  Diamond,
+  UpdateTime
+}

b461dd3ce05ccd3e67036ef2bcb3f144ea53c9cd
Zac Arellano <zacdarellano@gmail.com>
2017-11-10 21:40:01 -0800

    convert to es7 syntax

--- app/containers/Details/DetailsContainer.js
+++ app/containers/Details/DetailsContainer.js
@@ -2,29 +2,25 @@ import React, { Component } from 'react'
 import { Details } from 'components'
 
 class DetailsContainer extends Component {
-  constructor() {
-    super()
-    this.state = {
-      details: {},
-      panels: [],
-      activePanel: ''
-     }
-    this.switchPanel = this.switchPanel.bind(this)
-  }
+  state = { panels: [], activePanel: '' }
   componentDidMount() {
+    const defaultPanels = [ 'box score', 'team stats', 'leaders' ]
     this.setState({
-      panels: [ 'box score', 'team stats', 'leaders' ],
-      activePanel: 'box score'
+      panels: defaultPanels,
+      activePanel: defaultPanels[0]
     })
   }
-  switchPanel(newPanel) {
+  switchPanel = (newPanel) => {
     this.setState({ activePanel: newPanel })
   }
-  updateDetails(newDetails) {
-    this.setState({ details: newDetails })
-  }
   render() {
-    return <Details switchPanel={this.switchPanel} {...this.state} {...this.props} />
+    return (
+      <Details
+        {...this.state}
+        {...this.props}
+        switchPanel={this.switchPanel}
+      />
+    )
   }
 }
 

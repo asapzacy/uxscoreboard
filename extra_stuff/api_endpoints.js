206c39e4e9377de60495f9e063b85cbfc4fcbc76
Zac Arellano <zacdarellano@gmail.com>
2018-02-25 16:01:58 -0800

    Add to gitignore.

--- extra_stuff/api_endpoints.js
+++ extra_stuff/api_endpoints.js
@@ -1,4 +1,12 @@
 
+/**
+ * NFL endpoints -- live scores
+ */
+const nflAPi = `http://www.nfl.com/liveupdate/scores/scores.json`
 
-// NFL #2
-const api = `http://www.nfl.com/liveupdate/scores/scores.json`
+
+/**
+ * CBS endpoints -- team logos
+ */
+const primaryLogo = `http://sportsfly.cbsistatic.com/bundles/sportsmediacss/images/team-logos/${league}/${code.toUpperCase()}.svg`
+const alternativeLogo = `http://sportsfly.cbsistatic.com/bundles/sportsmediacss/images/team-logos/${league}/alt/${code.toUpperCase()}.svg`
